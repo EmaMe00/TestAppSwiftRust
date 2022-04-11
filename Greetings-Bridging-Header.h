808e04ad482d68d89f4e81e69a5012bfe47e6609
EmaMe00 <100135146+EmaMe00@users.noreply.github.com>
2022-04-11 15:37:02 +0200

    Add files via upload

---
+++ Greetings-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  Greetings-Bridging-Header.h
+//  TestApp
+//
+//  Created by Emanuele Mele on 11/04/22.
+//
+
+#ifndef Greetings_Bridging_Header_h
+#define Greetings_Bridging_Header_h
+
+#import "greetings.h"
+
+#endif
